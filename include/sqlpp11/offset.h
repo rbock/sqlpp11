3d777d982a8d7e36460027b8eaff8ee9d0676b90
Roland Bock <rb@theppro.com>
2013-09-27 10:59:09 +0200

    Added missing file offset.h

---
+++ include/sqlpp11/offset.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2013, Roland Bock
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ *   Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ *   Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SQLPP_OFFSET_H
+#define SQLPP_OFFSET_H
+
+#include <ostream>
+#include <sqlpp11/select_fwd.h>
+#include <sqlpp11/type_traits.h>
+
+namespace sqlpp
+{
+	struct offset_t
+	{
+		using _is_offset = tag_yes;
+
+		template<typename Db>
+			void serialize(std::ostream& os, Db& db) const
+			{
+				os << " OFFSET " << _offset;
+			}
+
+		const std::size_t _offset;
+	};
+
+	struct dynamic_offset_t
+	{
+		using _is_offset = tag_yes;
+		using _is_dynamic = tag_yes;
+
+		dynamic_offset_t():
+			_offset(0)
+		{}
+		dynamic_offset_t(const dynamic_offset_t&) = default;
+		dynamic_offset_t(dynamic_offset_t&&) = default;
+		dynamic_offset_t& operator=(const dynamic_offset_t&) = default;
+		dynamic_offset_t& operator=(dynamic_offset_t&&) = default;
+		~dynamic_offset_t() = default;
+
+		void set(std::size_t offset)
+		{
+			_offset = offset;
+		}
+
+		template<typename Db>
+			void serialize(std::ostream& os, Db& db) const
+			{
+				if (_offset > 0)
+					os << " OFFSET " << _offset;
+			}
+
+		std::size_t _offset = 0;
+	};
+
+}
+
+#endif
