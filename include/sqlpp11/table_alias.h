f478e7c8e860514c0b2ab94114e8799bd528b999
rbock <rbock@eudoxos.de>
2014-08-25 18:31:01 +0200

    Applied new member_t to remaining places

--- include/sqlpp11/table_alias.h
+++ include/sqlpp11/table_alias.h
@@ -36,7 +36,8 @@
 namespace sqlpp
 {
 	template<typename AliasProvider, typename Table, typename... ColumnSpec>
-		struct table_alias_t: public ColumnSpec::_name_t::template _member_t<column_t<AliasProvider, ColumnSpec>>...
+		struct table_alias_t:
+			public member_t<ColumnSpec, column_t<AliasProvider, ColumnSpec>>...
 	{
 		//FIXME: Need to add join functionality
 		using _traits = make_traits<value_type_of<Table>, tag::is_table, tag::is_alias, tag_if<tag::is_named_expression, is_expression_t<Table>::value>>;
