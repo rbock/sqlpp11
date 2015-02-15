9e1ecaf15a3702ed45b08bef0af87e1bb33a10a3
rbock <rbock@eudoxos.de>
2015-02-15 16:41:08 +0100

    Fixed weird compiler crash with gcc-4.8.2
    I have no idea why this is helping, but it does.
    
    I need to invest some time to simplify the templates so that compilers
    aren't as stressed with it.

--- include/sqlpp11/statement.h
+++ include/sqlpp11/statement.h
@@ -189,12 +189,17 @@ namespace sqlpp
 		using _consistency_check = detail::get_first_if<is_inconsistent_t, consistent_t, 
 					typename Policies::template _base_t<_policies_t>::_consistency_check..., 
 					typename _policies_t::_table_check>;
-		using _prepare_check = detail::get_first_if<is_inconsistent_t, consistent_t, 
-					typename _policies_t::_cte_check, 
-					_consistency_check>;
+
 		using _run_check = detail::get_first_if<is_inconsistent_t, consistent_t, 
 					typename _policies_t::_parameter_check, 
-					_prepare_check>;
+					typename _policies_t::_cte_check, 
+					typename Policies::template _base_t<_policies_t>::_consistency_check..., 
+					typename _policies_t::_table_check>;
+
+		using _prepare_check = detail::get_first_if<is_inconsistent_t, consistent_t,
+					typename _policies_t::_cte_check, 
+					typename Policies::template _base_t<_policies_t>::_consistency_check..., 
+					typename _policies_t::_table_check>;
 
 		using _result_type_provider = typename _policies_t::_result_type_provider;
 		template<typename Composite>
