6bdf5ef1ee03b8ec784fd396a98723e4d136c016
Aaron Bishop <erroneous@gmail.com>
2016-05-23 08:16:27 -0400

    updated copyright

--- include/sqlpp11/chrono.h
+++ include/sqlpp11/chrono.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2015, Roland Bock
+ * Copyright (c) 2015-2016, Roland Bock, Aaron Bishop
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
--- tests/sample.sql
+++ tests/sample.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, Roland Bock
+ * Copyright (c) 2013-2016, Roland Bock, Aaron Bishop
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
