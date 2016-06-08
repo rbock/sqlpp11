fe5f2ef2ef046606e52280c63148c392c0f5565e
Aaron Bishop <erroneous@gmail.com>
2016-06-07 20:43:22 -0400

    removed datetime and timestamp

--- include/sqlpp11/data_types/time_point/data_type.h
+++ include/sqlpp11/data_types/time_point/data_type.h
@@ -40,8 +40,5 @@ namespace sqlpp
     template <typename T>
     using _is_valid_operand = is_day_or_time_point_t<T>;
   };
-
-  using datetime = time_point;
-  using timestamp = time_point;
 }
 #endif
