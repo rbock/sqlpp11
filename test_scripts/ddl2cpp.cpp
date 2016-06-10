c26e3a8c30676cbf1a5bf5236a50a9ac7f32294a
strangeqargo <strangeqargo@gmail.com>
2016-06-11 02:10:14 +0300

    running python

--- test_scripts/ddl2cpp.cpp
+++ test_scripts/ddl2cpp.cpp
@@ -2,9 +2,13 @@
 //#include "MockDb.h" //todo 1
 #include "ddl2cpp_test.h"
 #include <iostream>
+#include <string>
 int ddl2cpp(int, char* [])
 {
   std::cout << test_scripts_pythonPath; //included from cmake-generated header if python is found
+  std::string args = " scripts/ddl2cpp  -fail-on-parse sample_ddl2cpp.sql";
+  auto python_args = test_scripts_pythonPath + args.c_str();
+  system(python_args.c_str());
   /* maybe todo 1: test compile / db_mock, for now we're just testing that ddl2cpp generates header without errors
    */
   /*
