73ca9d759fbff4deccfd776e09c3e21f7deb5581
strangeqargo <strangeqargo@gmail.com>
2016-06-11 01:36:42 +0300

    cmake vertigo

--- test_scripts/ddl2cpp.cpp
+++ test_scripts/ddl2cpp.cpp
@@ -1,6 +1,25 @@
 #include <sqlpp11/sqlpp11.h>
-
+//#include "MockDb.h" //todo 1
+#include "ddl2cpp_test.h"
+#include <iostream>
 int ddl2cpp(int, char* [])
 {
+  std::cout << test_scripts_pythonPath; //included from cmake-generated header if python is found
+  /* maybe todo 1: test compile / db_mock, for now we're just testing that ddl2cpp generates header without errors
+   */
+  /*
+  MockDb db = {};
+  MockDb::_serializer_context_t printer = {};
+
+  const auto f = test::TabFoo{};
+  const auto t = test::TabBar{};
+  const auto tab_a = f.as(sqlpp::alias::a);
+
+  getColumn(db, t._epsilon); //underscore bug check
+  */
+
   return 0;
 }
+
+
+
