4ae55e238e80c44e799743b95246363723aa37d8
Serge Robyns <serge.robyns@rc-s.be>
2017-04-10 11:12:27 +0200

    Changed copyright message

--- include/sqlpp11/for_update.h
+++ include/sqlpp11/for_update.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2016, Roland Bock
+ * Copyright (c) 2017, Serge Robyns
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
