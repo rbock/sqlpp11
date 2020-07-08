53b11dda017a8a0abfbf15fa53a7916fa66d8d1d
Daniel Evers <dermojo@gmail.com>
2020-07-08 21:15:24 +0200

    issue #342: added reference to clang bug ticket

--- include/sqlpp11/column.h
+++ include/sqlpp11/column.h
@@ -63,7 +63,8 @@ namespace sqlpp
     template <typename T>
     using _is_valid_assignment_operand = is_valid_assignment_operand<value_type_of<ColumnSpec>, T>;
 
-    // disambiguation for C++20
+    // disambiguation for C++20 / clang
+    // (see https://bugs.llvm.org/show_bug.cgi?id=46508)
     using expression_operators<column_t<Table, ColumnSpec>, value_type_of<ColumnSpec>>::operator==;
     using expression_operators<column_t<Table, ColumnSpec>, value_type_of<ColumnSpec>>::operator!=;
 
