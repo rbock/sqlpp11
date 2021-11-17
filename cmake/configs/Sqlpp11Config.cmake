d461eef051a148268f13b27a2a453bb36ac806f4
Leon De Andrade <leondeandrade@mailo.com>
2021-11-17 16:25:27 +0100

    Use sqlpp11 not sqlpp17

--- cmake/configs/Sqlpp11Config.cmake
+++ cmake/configs/Sqlpp11Config.cmake
@@ -34,19 +34,19 @@ endif()
 
 # Check all required components are available before trying to load any
 foreach(comp IN LISTS ${CMAKE_FIND_PACKAGE_NAME}_comps)
-    if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED_${comp} AND NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/Sqlpp17${comp}Config.cmake)
-        set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Sqlpp17 missing required component: ${comp}")
+    if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED_${comp} AND NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/Sqlpp11${comp}Config.cmake)
+        set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Sqlpp11 missing required component: ${comp}")
         set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
         return()
     endif()
 endforeach()
 
 # Add the target file
-include(${CMAKE_CURRENT_LIST_DIR}/Sqlpp17Targets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/Sqlpp11Targets.cmake)
 
 # Load any optional components 
 foreach(comp IN LISTS ${CMAKE_FIND_PACKAGE_NAME}_comps)
-    include(${CMAKE_CURRENT_LIST_DIR}/Sqlpp17${comp}Config.cmake OPTIONAL)
+    include(${CMAKE_CURRENT_LIST_DIR}/Sqlpp11${comp}Config.cmake OPTIONAL)
 endforeach()
 
 # Import "ddl2cpp" script
