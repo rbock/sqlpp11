8c00e921c01adff209f039710d7b328bc6a9430d
MeanSquaredError <35379301+MeanSquaredError@users.noreply.github.com>
2023-08-28 22:52:48 +0300

    Move the method definitions of sqlpp::connection_pool::pool_core inside the class. Change indentation style from K&R to Allman.

--- include/sqlpp11/connection_pool.h
+++ include/sqlpp11/connection_pool.h
@@ -44,7 +44,12 @@ namespace sqlpp
     class pool_core : public std::enable_shared_from_this<pool_core>
     {
     public:
-      pool_core(const _config_ptr_t& connection_config, std::size_t capacity);
+      pool_core(const _config_ptr_t& connection_config, std::size_t capacity) :
+        _connection_config{connection_config},
+        _handles{capacity}
+      {
+      }
+
       pool_core() = delete;
       pool_core(const pool_core &) = delete;
       pool_core(pool_core &&) = delete;
@@ -52,10 +57,40 @@ namespace sqlpp
       pool_core& operator=(const pool_core&) = delete;
       pool_core& operator=(pool_core&&) = delete;
 
-      _pooled_connection_t get();
-      void put(_handle_ptr_t& handle);
+      _pooled_connection_t get()
+      {
+        std::unique_lock<std::mutex> lock{_mutex};
+        if (_handles.empty())
+        {
+          lock.unlock();
+          return _pooled_connection_t{_connection_config, this->shared_from_this()};
+        }
+        auto handle = std::move(_handles.front());
+        _handles.pop_front();
+        lock.unlock();
+        // If the fetched connection is dead, drop it and create a new one on the fly
+        return
+          handle->check_connection() ?
+          _pooled_connection_t{std::move(handle), this->shared_from_this()} :
+          _pooled_connection_t{_connection_config, this->shared_from_this()};
+      }
+
+      void put(_handle_ptr_t& handle)
+      {
+        std::unique_lock<std::mutex> lock{_mutex};
+        if (_handles.full ())
+        {
+                _handles.set_capacity (_handles.capacity () + 5);
+        }
+        _handles.push_back(std::move(handle));
+      }
+
       // Returns number of connections available in the pool. Only used in tests.
-      std::size_t available();
+      std::size_t available()
+      {
+        std::unique_lock<std::mutex> lock{_mutex};
+        return _handles.size();
+      }
 
     private:
       _config_ptr_t _connection_config;
@@ -79,48 +114,6 @@ namespace sqlpp
   private:
     std::shared_ptr<pool_core> _core;
   };
-
-  template<typename ConnectionBase>
-  connection_pool<ConnectionBase>::pool_core::pool_core(const _config_ptr_t& connection_config, std::size_t capacity) :
-    _connection_config{connection_config},
-    _handles{capacity}
-  {
-  }
-
-  template<typename ConnectionBase>
-  typename connection_pool<ConnectionBase>::_pooled_connection_t connection_pool<ConnectionBase>::pool_core::get()
-  {
-    std::unique_lock<std::mutex> lock{_mutex};
-    if (_handles.empty()) {
-      lock.unlock();
-      return _pooled_connection_t{_connection_config, this->shared_from_this()};
-    }
-    auto handle = std::move(_handles.front());
-    _handles.pop_front();
-    lock.unlock();
-    // If the fetched connection is dead, drop it and create a new one on the fly
-    return
-      handle->check_connection() ?
-      _pooled_connection_t{std::move(handle), this->shared_from_this()} :
-      _pooled_connection_t{_connection_config, this->shared_from_this()};
-  }
-
-  template<typename ConnectionBase>
-  void connection_pool<ConnectionBase>::pool_core::put(_handle_ptr_t& handle)
-  {
-    std::unique_lock<std::mutex> lock{_mutex};
-    if (_handles.full ()) {
-            _handles.set_capacity (_handles.capacity () + 5);
-    }
-    _handles.push_back(std::move(handle));
-  }
-
-  template<typename ConnectionBase>
-  std::size_t connection_pool<ConnectionBase>::pool_core::available()
-  {
-    std::unique_lock<std::mutex> lock{_mutex};
-    return _handles.size();
-  }
 
   template<typename ConnectionBase>
   connection_pool<ConnectionBase>::connection_pool(const _config_ptr_t& connection_config, std::size_t capacity) :
