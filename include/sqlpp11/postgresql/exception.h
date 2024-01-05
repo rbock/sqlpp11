0cc45bd790cbc075cf6eb4bcdc6fe3fb85ef66c0
MeanSquaredError <35379301+MeanSquaredError@users.noreply.github.com>
2024-01-05 10:02:32 +0200

    PostgreSQL exceptions for serialization_failure and deadlock_detected (#551)
    * Add the exception sqlpp::postgresql::serialization_failure
    * Add the exception sqlpp::postgresql::deadlock_detected

--- include/sqlpp11/postgresql/exception.h
+++ include/sqlpp11/postgresql/exception.h
@@ -288,6 +288,28 @@ namespace sqlpp
       }
     };
 
+    class DLL_PUBLIC serialization_failure : public sql_error
+    {
+    public:
+      explicit serialization_failure(std::string err) : sql_error{std::move(err)}
+      {
+      }
+      serialization_failure(std::string err, std::string Q) : sql_error{std::move(err), std::move(Q)}
+      {
+      }
+    };
+
+    class DLL_PUBLIC deadlock_detected : public sql_error
+    {
+    public:
+      explicit deadlock_detected(std::string err) : sql_error{std::move(err)}
+      {
+      }
+      deadlock_detected(std::string err, std::string Q) : sql_error{std::move(err), std::move(Q)}
+      {
+      }
+    };
+
     class DLL_PUBLIC syntax_error : public sql_error
     {
     public:
